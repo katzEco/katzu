7272fcb37826d038b12049484ddecbbc6edfc1d8
dethMastery <me@suphakit.net>
2023-03-11 13:12:38 +0700

    added; package based

---
+++ bin/index.js
@@ -0,0 +1,26 @@
+let tydon = require('tydon')
+let rand = require('ran-string')
+
+exports.tyDon = function (inp) {
+  let epoch
+
+  if (inp != undefined) {
+    epoch = tydon.tyDon(inp)
+  } else {
+    epoch = tydon.tydon()
+  }
+
+  return epoch
+}
+
+exports.ranString = function (inp) {
+  let dString
+
+  if (inp != undefined) {
+    dString = rand.rand(inp)
+  } else {
+    dString = rand.rand(10)
+  }
+
+  return dString
+}
